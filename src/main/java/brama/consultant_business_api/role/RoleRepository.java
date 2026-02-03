ca81bdb684db4c62eb4f64f0ad827f42eeca2b5a
MURADU <iurie.muradu.ext@gmail.com>
2026-02-03 15:05:24 +0100

    fix issue

--- src/main/java/brama/consultant_business_api/role/RoleRepository.java
+++ src/main/java/brama/consultant_business_api/role/RoleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface RoleRepository extends MongoRepository<Role, String> {
-    Optional<Role> findByName(String roleUser);
+    Optional<Role> findFirstByName(String name);
+    boolean existsByName(String name);
 }
 
